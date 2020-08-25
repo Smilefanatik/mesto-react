b5d5e8c2d8e48d7665d7fa440f9f4136b22fd378
Smilefanatik <djsmilefanatik@yandex.ru>
2020-08-25 15:31:52 +0500

    Updates

---
+++ precache-manifest.493cb5c0ee8602e33402265346c9482b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e2963c2e0766e046cc5bc3a9378546",
+    "url": "/SmileFanatik/mesto-react/index.html"
+  },
+  {
+    "revision": "93574128586913d3975a",
+    "url": "/SmileFanatik/mesto-react/static/css/main.b2ca2d6c.chunk.css"
+  },
+  {
+    "revision": "55dd2ac55b05de455972",
+    "url": "/SmileFanatik/mesto-react/static/js/2.dcab687c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SmileFanatik/mesto-react/static/js/2.dcab687c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93574128586913d3975a",
+    "url": "/SmileFanatik/mesto-react/static/js/main.b78fc3e6.chunk.js"
+  },
+  {
+    "revision": "84b25c6ef42258e16856",
+    "url": "/SmileFanatik/mesto-react/static/js/runtime-main.b6afbc3d.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/SmileFanatik/mesto-react/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/SmileFanatik/mesto-react/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "35ee0c124d0e19a342243bcaa1ae2f48",
+    "url": "/SmileFanatik/mesto-react/static/media/Inter-Medium.35ee0c12.woff2"
+  },
+  {
+    "revision": "85f4b8e903fbd1d2c98a67dd0dca42e7",
+    "url": "/SmileFanatik/mesto-react/static/media/Inter-Medium.85f4b8e9.woff"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/SmileFanatik/mesto-react/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/SmileFanatik/mesto-react/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/SmileFanatik/mesto-react/static/media/add-button.b2fce4ac.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/SmileFanatik/mesto-react/static/media/bin-icon.d8078343.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/SmileFanatik/mesto-react/static/media/close-icon.5df32a06.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/SmileFanatik/mesto-react/static/media/edit-button-big.d476a8b8.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/SmileFanatik/mesto-react/static/media/edit-button.6bfdb354.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/SmileFanatik/mesto-react/static/media/heart-icon.a8acc871.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/SmileFanatik/mesto-react/static/media/heart-icon_black.5bbb05f7.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/SmileFanatik/mesto-react/static/media/logo.c6f11019.svg"
+  }
+]);
